834f7be6c2fb431bc8cf95b1656f461cba14b282
Lakshay_Edicts <77452480+LakshayEdicts@users.noreply.github.com>
2021-01-15 18:27:09 +0530

    Create pong_game.js

---
+++ pong_game.js
@@ -0,0 +1,56 @@
+var xBall = Math.floor(Math.random () *300) +50;
+var yBall = 50;
+var xSpeed = (2,7);
+var ySpeed = (-2,-7);
+var Score = 0
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background('#00D6FF');
+  fill('#0092ff');
+  rect(mouseX, 375, 90,15 );
+  move();
+  display();
+  bounce();
+  paddle();
+  // writing a code for displaying the Score
+  fill ('#FF0004');
+  textSize(25);
+  text("Score :" + Score , 10, 25);
+}
+//for the movement of ball and paddle
+function move() {
+  xBall += xSpeed;
+  yBall += ySpeed;
+}
+// for bouncing the ball
+function bounce(){
+  if(xBall < 10 ||
+    xBall > 400-10) {
+    xSpeed *= -1;
+  }
+  if (yBall < 10 ||
+     yBall > 400 - 10){
+    ySpeed *= -1;
+  }
+}
+
+// function is for displaying ball
+function display() {
+fill ('#F4FF00');
+ellipse (xBall , yBall, 20, 20)
+}
+
+// function for paddle 
+function paddle() {
+  if ((xBall > mouseX &&
+       xBall < mouseX +90) &&
+      (yBall +10 >= 375)) {
+    xSpeed *= -1;
+    ySpeed *= -1;
+    Score++;
+  }   
+}
